103c5edcd4a79a489811b4eeceedceb472551427
fluctlights <antoniofernando.mateo@alu.uclm.es>
2024-06-27 17:38:40 +0200

    Mejoras programa pruebas y stats completos

--- Codigo/Codigo-PRUEBAS/deploy.sh
+++ Codigo/Codigo-PRUEBAS/deploy.sh
@@ -7,52 +7,75 @@ else
 	sudo sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
 	rm benchmarks_deploy > /dev/null 2>&1
 
-	items=("D" "W" "p" "P")
-	sizes_dhrystone=("30000" "80000" "400000" "200000" "10000000")
-	sizes_whetstone=("300" "5500" "40000" "65000" "200000")
-	sizes_calcpi=("300000" "905000" "4000000" "25000000" "100000000")
-	sizes_calcprime=("3000" "9500" "40000" "250000" "1000000")
+	items=("W" "p" "P")
+	sizes_dhrystone=("60000" "120000" "240000" "480000" "960000")
+	sizes_whetstone=("3000" "6000" "12000" "24000" "48000")
+	sizes_calcpi=("300000" "600000" "1200000" "2400000" "4800000")
+	sizes_calcprime=("3000" "6000" "12000" "24000" "48000")
 	n_repetitions=$2
+	
+	gcc -O0 -static -DREPETITIONS=$n_repetitions benchmarks_deploy.c -o benchmarks_deploy -lm > /dev/null 2>&1
 
 	for item in "${items[@]}"
 	do
 		if [[ $item == "D" ]]; then
-			echo "Ejecutando benchmark Dhrystone"
-			gcc -O0 -DREPETITIONS=$n_repetitions benchmarks_deploy.c -o benchmarks_deploy -lm > /dev/null 2>&1
-
-			for size_dhrystone in "${sizes_dhrystone[@]}"
+			for size in "${sizes_dhrystone[@]}" 
 			do
-				./benchmarks_deploy -b $item -s $size_dhrystone
+				echo ""
+				echo "LISTO? [yes]"
+				read decision
+				echo ""
+				echo "Ejecutando benchmark Dhrystone"
+			
+				for i in {1..5}; do
+					./benchmarks_deploy -b $item -s $size
+				done
+				sleep 1
 			done
 		fi
 
 		if [[ $item == "W" ]]; then
-			echo "Ejecutando benchmark Whetstone"
-			gcc -O0 -DREPETITIONS=$n_repetitions benchmarks_deploy.c -o benchmarks_deploy -lm > /dev/null 2>&1
-
-			for size_whetstone in "${sizes_whetstone[@]}"
+			for size in "${sizes_whetstone[@]}" 
 			do
-				./benchmarks_deploy -b $item -s $size_whetstone
+				echo "Ejecutando benchmark Whetstone"
+				echo ""
+                        	echo "LISTO? [yes]"
+                        	read decision
+                        	echo ""
+				for i in {1..5}; do
+					./benchmarks_deploy -b $item -s $size
+				done
+				sleep 1
 			done
 		fi
 
 		if [[ $item == "p" ]]; then
-			echo "Ejecutando benchmark Calculo de Pi"
-			gcc -O0 -DREPETITIONS=$n_repetitions benchmarks_deploy.c -o benchmarks_deploy -lm > /dev/null 2>&1
-
-			for size_calcpi in "${sizes_calcpi[@]}"
+			for size in "${sizes_calcpi[@]}" 
 			do
-				./benchmarks_deploy -b $item -s $size_calcpi
+				echo "Ejecutando benchmark Calculo de Pi"
+				echo ""
+                        	echo "LISTO? [yes]"
+                        	read decision
+                        	echo ""
+				for i in {1..5}; do
+					./benchmarks_deploy -b $item -s $size
+				done
+				sleep 1
 			done
 		fi
 
 		if [[ $item == "P" ]]; then
-			echo "Ejecutando benchmark Calculo de primos"
-			gcc -O0 -DREPETITIONS=$n_repetitions benchmarks_deploy.c -o benchmarks_deploy -lm > /dev/null 2>&1
-
-			for size_calcprime in "${sizes_calcprime[@]}"
+			for size in "${sizes_calcprime[@]}"
 			do
-				./benchmarks_deploy -b $item -s $size_calcprime
+				echo "Ejecutando benchmark Calculo de primos"
+				echo ""
+                        	echo "LISTO? [yes]"
+                        	read decision
+                        	echo ""
+				for i in {1..5}; do
+					./benchmarks_deploy -b $item -s $size
+				done
+				sleep 1
 			done
 		fi
 	done
